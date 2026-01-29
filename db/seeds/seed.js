b2e497b63e23e1c77648b3bf04b6f24e9deec86e
Yanzhao Chen <yanzhaochen@Yanzhaos-MacBook-Pro-2.local>
2026-01-29 12:50:59 +0000

    Task 1 & 2: setup and create tables

--- db/seeds/seed.js
+++ db/seeds/seed.js
@@ -1,6 +1,49 @@
-const db = require("../connection")
+const db = require("../connection");
 
-const seed = ({ topicData, userData, articleData, commentData }) => {
-  return db.query(); //<< write your first query in here.
+const seed = async ({ topicData, userData, articleData, commentData }) => {
+  await db.query(`DROP TABLE IF EXISTS comments;`);
+  await db.query(`DROP TABLE IF EXISTS articles;`);
+  await db.query(`DROP TABLE IF EXISTS users;`);
+  await db.query(`DROP TABLE IF EXISTS topics;`);
+
+  await db.query(`
+    CREATE TABLE topics (
+      slug VARCHAR PRIMARY KEY,
+      description VARCHAR,
+      img_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE users (
+      username VARCHAR PRIMARY KEY,
+      name VARCHAR,
+      avatar_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE articles (
+      article_id SERIAL PRIMARY KEY,
+      title VARCHAR,
+      topic VARCHAR REFERENCES topics(slug),
+      author VARCHAR REFERENCES users(username),
+      body TEXT,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      votes INT DEFAULT 0,
+      article_img_url VARCHAR(1000)
+    );
+  `);
+
+  await db.query(`
+    CREATE TABLE comments (
+      comment_id SERIAL PRIMARY KEY,
+      article_id INT NOT NULL REFERENCES articles(article_id),
+      body TEXT,
+      votes INT DEFAULT 0,
+      author VARCHAR REFERENCES users(username),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );
+  `);
 };
 module.exports = seed;
